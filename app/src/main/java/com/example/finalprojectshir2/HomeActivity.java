8924fae9d32186588c4e3fdc8cb39a6d8cba8bfd
ShirAvezof <shir00avezof@gmail.com>
2024-11-14 11:47:20 +0200

    ad

--- app/src/main/java/com/example/finalprojectshir2/HomeActivity.java
+++ app/src/main/java/com/example/finalprojectshir2/HomeActivity.java
@@ -8,13 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HomeActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-
+        bottomNavigationView = findViewById(R.id.nav_home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.bottomNavigationView);
     }
 }
--- app/src/main/java/com/example/finalprojectshir2/ParentProfileActivity.java
+++ app/src/main/java/com/example/finalprojectshir2/ParentProfileActivity.java
@@ -8,17 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class ParentProfileActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_parent_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+    bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    bottomNavigationView.setSelectedItemId(R.id.);
     }
 }
