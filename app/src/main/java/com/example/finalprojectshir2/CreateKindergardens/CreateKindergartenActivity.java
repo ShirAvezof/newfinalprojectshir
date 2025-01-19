27c5f73c4d61cbfca89cebbbac718d528f4350b0
ShirAvezof <shir00avezof@gmail.com>
2025-01-19 14:22:57 +0200

    פתחתי repository presenter callback של create gan

--- app/src/main/java/com/example/finalprojectshir2/CreateKindergardens/CreateKindergartenActivity.java
+++ app/src/main/java/com/example/finalprojectshir2/CreateKindergardens/CreateKindergartenActivity.java
@@ -1,26 +1,49 @@
 package com.example.finalprojectshir2.CreateKindergardens;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.finalprojectshir2.MainActivity;
 import com.example.finalprojectshir2.Parent.Register.RegisterPresenter;
 import com.example.finalprojectshir2.R;
 import com.example.finalprojectshir2.models.KinderGarten;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class CreateKindergartenActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class CreateKindergartenActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText kindergartenNameEditText, ownerNameEditText, addressEditText, phoneEditText, aboutEditText, hoursEditText;
-    private Button submitButton;
+    private Button submitButton,uploadPhotosButton;
+    private ImageView imageView;
     private FirebaseFirestore firestore;
     private FirebaseAuth mAuth;
     private CreateKindergartenPresenter presenter;
+    private static final String IMAGE_DIRECTORY = "/demonuts";
+    private int GALLERY = 1, CAMERA = 2;
+    //  UserCarOpenHelper ucoh;
+    final int REQUEST_CODE_GALLERY = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +61,13 @@ public class CreateKindergartenActivity extends AppCompatActivity {
         aboutEditText = findViewById(R.id.aboutEditText);
         hoursEditText = findViewById(R.id.hoursEditText);
         submitButton = findViewById(R.id.submitButton);
+        uploadPhotosButton = findViewById(R.id.uploadPhotosButton);
+        imageView = findViewById(R.id.imageView2);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String kindergartenName = kindergartenNameEditText.getText().toString().trim();
                 String ownerName = ownerNameEditText.getText().toString().trim();
                 String address = addressEditText.getText().toString().trim();
@@ -57,11 +83,111 @@ public class CreateKindergartenActivity extends AppCompatActivity {
             }
         });
 
+        uploadPhotosButton.setOnClickListener(this );
+
+
+
+
+
+
+    }
+    private void showPictureDialog(){
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle("נא לבחור מאיפה להוסיף תמונה:");
+        String[] pictureDialogItems = {
+                "מהגלריה",
+                "מהמצלמה" };
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                choosePhotoFromGallary();
+                                break;
+                            case 1:
+                                takePhotoFromCamera();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+
+
+
+    public void choosePhotoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+
+    // ============================
+
+
+    private void takePhotoFromCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+
+
+
+
+
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+        }
+
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+    }
+
+
+
+
+    //Convert imageView to byte[]
+    private byte[] imageViewToByte(ImageView image) {
+        Bitmap bitmap=((BitmapDrawable)image.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 0, stream);
+        byte[]byteArray=stream.toByteArray();
+        return byteArray;
     }
     public void showSuccess(KinderGarten garten) {
         Toast.makeText(this, "kinderGarten added successfully", Toast.LENGTH_SHORT).show();
     }
     public void showError(String message) {
         Toast.makeText(this, "kinderGarten did not add", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        showPictureDialog();
     }
 }
